f383a328a7ee4a915e7858fe25d02deac5d17d7b
Brenton Alker <brenton@tekerson.com>
2014-09-19 19:02:49 +1000

    Add logic for moving in various directions

--- pybots/headings.py
+++ pybots/headings.py
@@ -6,6 +6,14 @@ headings = [
 ]
 
 
+move_map = {
+    "NORTH": (0, 1),
+    "EAST": (1, 0),
+    "SOUTH": (0, -1),
+    "WEST": (-1, 0),
+}
+
+
 def right_of(start):
     pos = headings.index(start)
     try:
@@ -20,3 +28,11 @@ def left_of(start):
         return headings[pos - 1]
     except IndexError:
         return headings[0]
+
+
+def move(heading, loc):
+    return add_elementwise(loc, move_map[heading])
+
+
+def add_elementwise(a, b):
+    return tuple(sum(x) for x in zip(a,b))
--- tests/pybots/headings_tests.py
+++ tests/pybots/headings_tests.py
@@ -16,3 +16,43 @@ def test_turn_right():
 def test_turn_left():
     for left, right in turns:
         assert_equals(headings.left_of(right), left)
+
+
+def test_move_north():
+    [(yield check_move_north, x, y)
+        for x in xrange(5)
+        for y in xrange(5)]
+
+
+def check_move_north(x, y):
+    assert_equals(headings.move("NORTH", (x, y)), (x, y + 1))
+
+
+def test_move_south():
+    [(yield check_move_south, x, y)
+        for x in xrange(5)
+        for y in xrange(5)]
+
+
+def check_move_south(x, y):
+    assert_equals(headings.move("SOUTH", (x, y)), (x, y - 1))
+
+
+def test_move_east():
+    [(yield check_move_east, x, y)
+        for x in xrange(5)
+        for y in xrange(5)]
+
+
+def check_move_east(x, y):
+    assert_equals(headings.move("EAST", (x, y)), (x + 1, y))
+
+
+def test_move_west():
+    [(yield check_move_west, x, y)
+        for x in xrange(5)
+        for y in xrange(5)]
+
+
+def check_move_west(x, y):
+    assert_equals(headings.move("WEST", (x, y)), (x - 1, y))
