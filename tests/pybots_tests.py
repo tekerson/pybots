024e4675c3d53a1849fba6ac4fdc729e5cb6cbbc
Brenton Alker <brenton@tekerson.com>
2014-09-20 19:18:50 +1000

    Move tests

--- tests/pybots_tests.py
+++ tests/pybots_tests.py
@@ -1,11 +1,13 @@
-import pybots
+import pybots.command as cmd
+import pybots.bot
+
 
 from nose.tools import assert_equals
 
 
-def setup():
-    pass
+def test_basic():
+    bot = pybots.bot.Bot()
+    cmd.parse("PLACE 0,0,NORTH")(bot)
+    cmd.parse("MOVE")(bot)
+    assert_equals(bot._location, (0, 1))
 
-
-def teardown():
-    pass
